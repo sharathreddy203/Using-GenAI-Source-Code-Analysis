e73b4082c9ac5a54657b445b3b6829308363b069
sharathreddy203 <sharathreddy2030@gmail.com>
2025-04-09 11:31:28 +0530

    updatedthe code

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'M Sharath Reddy',
+    author_email= 'sharathreddy2030@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
